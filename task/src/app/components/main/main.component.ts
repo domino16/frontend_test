879b860631461d916b7e12cfc2ffb295d2937f09
domin <domino1661@gmail.com>
2024-07-10 12:58:18 +0200

    initialized components

---
+++ task/src/app/components/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FirstBlockComponent } from "./first-block/first-block.component";
+import { SecondBlockComponent } from "./second-block/second-block.component";
+import { ThirdBlockComponent } from "./third-block/third-block.component";
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [FirstBlockComponent, SecondBlockComponent, ThirdBlockComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
